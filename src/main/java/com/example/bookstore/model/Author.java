36786f12b2c272dd7641d25c17951a8ff8569a4b
Cristian <39212816+sher009@users.noreply.github.com>
2024-03-22 12:18:50 +0100

    Added new attribute country

--- src/main/java/com/example/bookstore/model/Author.java
+++ src/main/java/com/example/bookstore/model/Author.java
@@ -1,4 +1,57 @@
 package com.example.bookstore.model;
 
-public class Author {
+public class Author{
+    // Поля класу; class attributes
+    private String firstName;
+    private String lastName;
+    private String birthDate;
+    private String country;
+    private String language;
+
+
+    //Конструктор; Constructor
+    public Author(String firstName, String lastName, String birthDate, String country, String language){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.country = country;
+        this.language = language;
+    }
+
+    //getters setters
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
 }
--- src/main/java/com/example/bookstore/model/MainAuthor.java
+++ src/main/java/com/example/bookstore/model/MainAuthor.java
@@ -1,2 +1,12 @@
-package com.example.bookstore.model;public class MainAuthor {
+package com.example.bookstore.model;
+
+public class MainAuthor {
+    public static void main(String[] args) {
+        // створюю нового автора; create new author
+        Author author = new Author("Igor", "Omega", "1998.12.12", "Moldova", "English");
+
+        System.out.println("Author: " + author.getFirstName() + " " + author.getLastName());
+        System.out.println("Birth Date: " + author.getBirthDate());
+        System.out.println("Language: " + author.getLanguage());
+    }
 }
