d38487e2ddb3aec5fde48002006780a403b32009
kristian <sherlok019@gmail.com>
2025-02-28 14:50:12 +0100

    put folders in the right depositories connect liquidbase and postgres together

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,14 @@
+import com.example.bookstore.model.Author;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // створюю нового автора; create new author
+        Author author = new Author("Igor", "Omega", LocalDate.of(1998,12,12), "Moldova", "English");
+
+        System.out.println("Author: " + author.getFirstName() + " " + author.getLastName());
+        System.out.println("Birth Date: " + author.getBirthDate());
+        System.out.println("Language: " + author.getLanguage());
     }
 }
